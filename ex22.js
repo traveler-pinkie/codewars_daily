f073128cc2e213764c9c279dc2b6639bb8e775a1
James Barrera <75712292+traveler-pinkie@users.noreply.github.com>
2023-02-28 20:02:14 -0500

    Update ex22.js

--- ex22.js
+++ ex22.js
@@ -5,10 +5,6 @@ function kaCokadekaMe(word){
 
     for(let i = 0; i < wordArr.length; i++){
         arr.push(word[i])
-        // first condition checks if the current letter is a vowel
-        //second condition checks if the next letter is not a vowel
-        // third condition check if i + 1 is not equal to the word length
-        // if all are true, then push ka to the array
         if(vowels.includes(wordArr[i]) && !vowels.includes(word[i+1]) && i + 1 != word.length){
             arr.push('ka')
         }
